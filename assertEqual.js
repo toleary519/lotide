ead093cd844c1341b360e2813ccf6ca4a35ba849
vagrant <t.oleary@me.com>
2021-07-19 23:24:14 +0000

    assertEqual function

---
+++ assertEqual.js
@@ -0,0 +1,10 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log("Assertion Failed");
+  } else {
+    console.log("Assertion Passed");
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
