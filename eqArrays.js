4724b8788eeb7fef3445c97d92a48613c8044ede
vagrant <t.oleary@me.com>
2021-07-21 02:16:52 +0000

    returned to normal

--- eqArrays.js
+++ eqArrays.js
@@ -21,12 +21,12 @@ return true;
 // do this for all items in the list
 
 
-assertEqual(eqArrays([1, 2, 100], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
 
 
 console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
 console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
 
-eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
 
