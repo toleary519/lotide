4f264874542a62fec7134fd682d2d9b2577d0df4
vagrant <t.oleary@me.com>
2021-07-22 03:41:01 +0000

    letter positions function with loopNum

---
+++ letterPositions.js
@@ -0,0 +1,20 @@
+
+const letterPositions = function(string) {
+  let output = {};
+  let loopNum = 0;
+  let spacelessString = string.split("").join(""); 
+  
+    for (let letter of spacelessString) {
+      if (letter !== " ") {
+        if (output[letter]) {
+          output[letter].push(loopNum);
+        } else {
+          output[letter] = [loopNum];
+        }
+      }
+      loopNum++;  
+    }
+  return output;
+};
+
+console.log(letterPositions("lighthouse in the house"));
