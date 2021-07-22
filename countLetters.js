cc9f13210481e4c950023eb2222233b2168fd58c
vagrant <t.oleary@me.com>
2021-07-22 02:14:29 +0000

    count letters function

---
+++ countLetters.js
@@ -0,0 +1,21 @@
+
+const countLetters = function(string) { 
+
+let output = {};
+//loopNum = 0;
+let spacelessString = string.split(" ").join(""); 
+
+  for (let letter of spacelessString) {
+    if (output[letter]) {
+      output[letter] += 1;
+      //console.log("if output:", output);
+    } else {
+      output[letter] = 1;
+      //console.log("else:", letter)
+    }
+  }
+return output;
+};
+// console.log(countLetters("LHL"));
+// console.log(countLetters("thomas"));
+// console.log(countLetters("hello hello"));
