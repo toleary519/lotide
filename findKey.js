cceb5be085c6b73cb5201adc4650f3d024585c8c
vagrant <t.oleary@me.com>
2021-07-23 03:10:17 +0000

    find key callback

---
+++ findKey.js
@@ -0,0 +1,44 @@
+const findKey = function(object, callback) {
+valList = Object.values(object);
+keyList = Object.keys(object);
+
+  for (let key of keyList) {
+       if (callback(object[key])) {
+        return key;
+        //console.log(object[val]); 
+        //output.push();
+         //break;
+       }
+  }
+}; 
+
+const result1 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 1) 
+
+const result2 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) 
+
+const result3 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 3) 
+
+console.log(result1); // => "Blue Hill"
+console.log(result2); // => "noma"
+console.log(result3); // => "Akaleri"
