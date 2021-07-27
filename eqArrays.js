b62bc025b34ad6722cbc1c25dae2b4943d3f9b06
vagrant <t.oleary@me.com>
2021-07-27 02:22:32 +0000

    module update

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,5 @@
 
-const assertEqual = function(actual, expected) {
-  if (actual !== expected) {
-    console.log("Assertion Failed");
-  } else {
-    console.log("Assertion Passed");
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const eqArrays = function(arrayOne, arrayTwo) {
 
@@ -17,16 +11,4 @@ const eqArrays = function(arrayOne, arrayTwo) {
 return true;
 };
 
-// take the item in first index of list one and list two and confirm they are the same
-// do this for all items in the list
-
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-
-
-console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
-
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
-
+module.exports = eqArrays;
--- tail.js
+++ tail.js
@@ -1,24 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (actual !== expected) {
-    console.log("Assertion Failed");
-  } else {
-    console.log("Assertion Passed");
-  }
-};
+const assertEqual = require('./assertEqual')
 
 const tail = function(array) {
   return array.slice(1);
 };
 
-let array = ["Hello", "Lighthouse", "Labs"];
-let result = tail(["Hello", "Lighthouse", "Labs"]);
-
-//console.log(tail(["Hello", "Lighthouse", "Labs"]));
-//assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
-
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-console.log(array);
-console.log(result);
-assertEqual(array, result);
+module.exports = tail;
